03ce0a10095d531628db0de24962fe1d4a5763cb
Jereme Evans <jereme@jeremeevans.com>
2022-08-25 10:32:57 -0700

    Initial header

--- rollup.config.ts
+++ rollup.config.ts
@@ -8,7 +8,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve"
 import { terser } from "rollup-plugin-terser"
 
 export default defineConfig({
-  input: "./src/example-element.ts",
+  input: "./src/site-header.ts",
   plugins: [
     nodeResolve(),
     typescript(),
---
+++ src/site-header.ts
@@ -0,0 +1,53 @@
+import { LitElement, html } from "lit"
+import { customElement, state } from "lit/decorators.js"
+import { classMap } from "lit/directives/class-map.js"
+import elementStyles from "./site-header.scss"
+import "./menu-button"
+
+/**
+ * A site header with an animated menu button.
+ *
+ * @slot - Site title and/or logo with link to root.
+ */
+@customElement("site-header")
+export class SiteHeader extends LitElement {
+  static override styles = elementStyles
+  @state() private menuOpen = false
+
+  override render() {
+    const { menuOpen } = this
+    const navClasses = { open: menuOpen }
+
+    return html`
+      <header>
+        <slot name="site-name"></slot>
+
+        <nav class=${classMap(navClasses)}>
+          <slot name="nav-links"></slot>
+        </nav>
+
+        <menu-button
+          id="mobile-menu-button"
+          @opened=${this.openNav}
+          @closed=${this.closeNav}
+        ></menu-button>
+      </header>
+    `
+  }
+
+  private openNav() {
+    this.menuOpen = true
+    document.body.classList.add("menu-open")
+  }
+
+  private closeNav() {
+    this.menuOpen = false
+    document.body.classList.remove("menu-open")
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "site-header": SiteHeader
+  }
+}
